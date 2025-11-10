e2107489136c4ae0327ec21ad5a04b61a3d9ce4c
yazeedkfupm <s202164390@kfupm.edu.sa>
2025-11-10 09:32:23 +0300

    task 4 9:32

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,19 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+   if (numbers.slice(1).includes(0)) {
+    return NaN; // or "Error: Division by zero"
+  }
+  return numbers.slice(1).reduce((result, num) => result / num, numbers[0]);
 }
 
